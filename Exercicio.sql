0b55fff52f53ef52b91f32c733ef5edb654c6eaa
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2022-12-11 12:13:26 -0300

    upload

--- Exercicio.sql
+++ Exercicio.sql
@@ -1,6 +1,6 @@
 --Exercicio
 SELECT DISTINCT	name
-	FROM tabela
+FROM tabela
 	ORDER BY col1;
 
 
@@ -13,22 +13,54 @@ SELECT *
 
 --Exercicio
 SELECT firstname
-	FROM staff
+FROM staff
 	WHERE salary
 		BETWEEN 1500 AND 1900;
 
 
 --Exercicio
 SELECT id, name
-	FROM students
+FROM students
 	WHERE id
 		BETWEEN 13 AND 45;
 
 --Exercicio
 SELECT *
-	FROM people
+FROM people
 	WHERE city = 'Boston';
 
+--Exercicio
+SELECT id, age, name
+FROM tabela
+	WHERE age >= 30 AND age <= 40;
+
+--Exercicio
+SELECT name
+FROM films
+	WHERE production = 'Marvel' AND yyear >= 2010
+	ORDER BY name;
+
+--Exercicio
+SELECT teamname, country
+FROM teams
+	WHERE country IN ('England', 'Spain', 'Germany');
+
+--Exercicio
+SELECT CONCAT (firstname, ' ', lastname) AS fullname,
+       salary*12+experience*500 AS total
+FROM staff
+	ORDER BY total;
+
+--Exercicio
+SELECT AVG(score)
+FROM sam_grades
+	WHERE semester=1;
+
+--Exercicio
+SELECT *
+FROM Foods
+	WHERE fatpercentage < (SELECT AVG(fatpercentage)
+						   FROM Foods);
 
 
 
--- SQL.sql
+++ SQL.sql
@@ -1,34 +1,37 @@
 --Chamar tabela
 SELECT col1, col2
-	FROM tabela;
+FROM tabela;
 --ou
 SELECT tabela.col1, tabela.col2
-	FROM tabela;
+FROM tabela;
 
 
 --Limita a quantidade de linhas
 SELECT col1, col2
-	FROM tabela
+FROM tabela
 	LIMIT 5;
 
 
 --Chama tabela a partir da linha X e puxando Y linhas
 SELECT col1, col2
-	FROM tabela
+FROM tabela
 	OFFSET 3 LIMIT 5;
 	--ou LIMIT 3, 5;
 
-
+/*
+ORDER BY col ASC  :crescente
+ORDER BY col DESC :decrescente
+*/
 --Ordenação
 SELECT *
-	FROM tabela
+FROM tabela
 	ORDER BY col1;
 	--ou ORDER BY col1, col2;
 
 
 --Chama tabela sob condição
 SELECT col1
-	FROM tabela
+FROM tabela
 	WHERE condicao;
 /*
 =  :igual
@@ -38,4 +41,41 @@ SELECT col1
 >= :maior ou igual
 <= :menor ou igual
 BETWEEN X AND Y :entre X e Y
+
+AND :e
+OR  :ou
+IN  :em
+NOT :não 
 */
+
+
+--IN (seleciona os que contém) / NOT (seleciona se não conter)
+SELECT *
+FROM tabela
+	WHERE city IN ('A', 'B', 'C'); --case sensitive
+	/*--ou
+	WHERE city IN NOT ('A', 'B', 'C'); --case sensitive
+	*/
+
+
+--CONCAT (concatena) e AS ("renomeia coluna")
+SELECT CONCAT (name1, ' ', name2) as namefull
+FROM tabela;
+
+
+/*
+Funções
+UPPER(col1) :deixa as letras maiúsculas
+LOWER(col2) :deixa as letras minúsculas
+SQRT(col3)  :raiz quadrada
+AVG(col4)   :média
+SUM(col5)   :soma
+*/
+
+--subconsulta
+SELECT name1, salary
+FROM employes
+	WHERE salary > (SELECT AVG(salary)
+					FROM employes)
+	ORDER BY salary DESC;
+
